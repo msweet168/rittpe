7a02a9326ac37141c8caa2d8e733b8939c2a85fe
msweet168 <mitchellsweet168@aol.com>
2018-01-21 20:10:33 -0500

    Add env virables for OpenShift

---
+++ sitewide/config.php
@@ -0,0 +1,8 @@
+<?php
+	return [
+	    'host' => getenv("TPE_DB_HOST"),
+	    'name' => getenv("TPE_DB_NAME"),
+	    'user' => getenv("TPE_DB_USER"),
+	    'pass' => getenv("TPE_DB_PASSWORD")
+	];
+?>
