539be2218e53dd659a560dc456f0be7d481cc9c6
msweet168 <mitchellsweet168@aol.com>
2018-03-29 19:19:21 -0400

    Added google calendar to events page.

--- events.php
+++ events.php
@@ -13,7 +13,7 @@
 	<h1 class="pageTitleWhite">Future Events</h1>
 </div>
 
-
+<iframe id="calendar" src="https://calendar.google.com/calendar/embed?src=g.rit.edu_d76ki9svr7m7mba13sohsdgu54%40group.calendar.google.com&ctz=America%2FNew_York" style="border: 0" width="800" height="600" frameborder="0" scrolling="no"></iframe>
 
 
 <?php 
